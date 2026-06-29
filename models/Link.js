064246fe285b631ba63b64c33a3cc3192f4fdea8
Juan Castro <juamcastroh@gmail.com>
2026-06-29 18:40:53 -0300

    feat: Agregar funciones para leer y guardar datos en la BD

--- models/Link.js
+++ models/Link.js
@@ -1,3 +1,25 @@
+const fs = require("fs");     // fs -> File System: Libreria que trabaja con archivos.
+const path = require("path"); // Sirve para construir rutas de archivos.
+
+// --- Construir ruta adsoluta completa a la base de datos ---
+const dbPath = path.join(__dirname, "../data/db.json");
+
+// -----------------------------------
+// Funcion: Para Leer BD 
+// -----------------------------------
+const getDB = () => {
+    return JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+};
+
+
+// -----------------------------------
+// Funcion: Para Guardar Datos
+// -----------------------------------
+const saveDB = (data) => {
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+};
+
+
 // --- Modelo de un link ---
 class Link {
 
