df018d2106e12ac0d417eb4e7684b5cf7f2fa61a
Juan Castro <juamcastroh@gmail.com>
2026-06-29 06:59:13 -0300

    feat: Implementar logica para obtener un tema por su ID

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -9,3 +9,14 @@ exports.getAllTopics = (req, res) => {
     
 }
 
+
+// --- Ver un tema por ID
+exports.getTopicById = (req, res) => {
+    const topic = Topic.findById(req.params.id);        // Obtener tema por Id.
+
+    if (!topic) return res.send("Topic no encontrado"); // Verificar si existe.
+
+    res.render("topics/show", { topic });               // Pasarle el tema a show.
+}
+
+
