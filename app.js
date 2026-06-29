934e50ac49c2cbd3f920d57c49dd1dc541285e9b
Juan Castro <juamcastroh@gmail.com>
2026-06-29 04:14:03 -0300

    feat: Configurar express y conectar rutas principales

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const express = require("express");
 
 const app = express();
 
+// -------------------------------------
+// IMPORTAR ROUTERS
+// -------------------------------------
+const topicRoutes = require("./routes/topicRoutes");
+const authRoutes = require("./routes/authRoutes");
+const linkRoutes = require("./routes/linkRoutes");
+const voteRoutes = require("./routes/voteRoutes");
+
+
+app.set("view engine", "ejs"); // Las vistan van a usar EJS.
+app.set("views", "./views");   // Posicion de la carpeta.
+
+
+// --- Transformar texto plano HTML a objeto js ---
+app.use(express.urlencoded({extended: true})); // HTML a objeto js - Datos complejos.
+app.use(express.json());                       // JS en formato json.
+
+// --- Enviar archivos en public al navegador ---
+app.use(express.static("public"));
+
+
+// --- Configuracion de rutas ---
+app.use("/topics", topicRoutes);
+app.use("/login", authRoutes);
+app.use("/link", linkRoutes);
+app.use("/votes", voteRoutes);
+
+
+// --- Servidor escuchando en el puerto 3000 ---
 app.listen(3000, () => {
-    console.log("Server running");
+    console.log("Servidor corriendo en el puerto 3000");
+    console.log("Hola")
+});
+
+// --- Ruta de prueba ---
+app.get("/", (req, res) => {
+    res.send("CRUD");
 });
