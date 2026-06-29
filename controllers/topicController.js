fcec5f0f6060fc8c5071e12a0349573a4caa0ea4
Juan Castro <juamcastroh@gmail.com>
2026-06-29 06:58:33 -0300

    feat: Terminar logica para obtener todos los temas

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,5 +1,11 @@
+// --- Importar Modelo de los temas ---
+const Topic = require("../models/Topic");
+
+
 // --- Logica para ver todos los temas ---
 exports.getAllTopics = (req, res) => {
-    res.render("topics/index");
-    console.log("Controldor funcionando");
+    const topics = Topic.findAll();         // Obtenemos todos los temas
+    res.render("topics/index", { topics }); // Pasamos los temas al view.
+    
 }
+
