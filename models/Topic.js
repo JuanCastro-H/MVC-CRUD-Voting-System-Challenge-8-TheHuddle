8970cb1916d7592198a8566d482c9ca9fb3f00cb
Juan Castro <juamcastroh@gmail.com>
2026-06-29 05:11:27 -0300

    feat: Crear funciones para leer y guardar datos en la BD

--- models/Topic.js
+++ models/Topic.js
@@ -1,5 +1,31 @@
-// --- Modelo de un tema ---
+const fs = require("fs");     // fs -> File System: Libreria que trabaja con archivos.
+const path = require("path"); // Sirve para construir rutas de archivos.
+
+// --- Construir ruta completa a la base de datos ---
+const dbPath = path.join(__dirname, "../data/db.json");
+
+
+// -----------------------------------
+// Funcion: Para Leer BD 
+// -----------------------------------
+const getDB = () => {
+    return JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+};
+
+
+// -----------------------------------
+// Funcion: Para Guardar Datos
+// -----------------------------------
+const saveDB = (data) => {
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+};
+
+
+
+
 class Topic {
+
+
 
 }
 
