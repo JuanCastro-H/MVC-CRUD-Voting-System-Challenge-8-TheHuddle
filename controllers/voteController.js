a3960a842f6ac0c5bab75aa8b96d759f9166cb53
Juan Castro <juamcastroh@gmail.com>
2026-06-30 02:33:45 -0300

    feat: Crear logica para poder votar un Link

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -4,16 +4,26 @@ const Topic = require("../models/Topic");
 // --- Importar Modelo de Links --
 const Link = required("../models/Link");
 
-// --- Logica Para Votar Por un Tema ---
+
+// --- Logica Para Votar Un Tema ---
 exports.VoteTopic = (req, res) => {
 
     // --- Aumentar Voto del Tema ---
     Topic.vote(req.params.id);
 
-    // --- Redireccionar a La Pagina de Temas ---
+    // --- Redireccionar a Lista de Temas ---
     res.redirect("/topics");
 
 };
 
 
+// --- Logica Para Votar Un Link ---
+exports.voteLink = (req, res) => {
+    
+    // --- Obterner Y Aumentar voto Del Link ---
+    const link = Link.vote(req.params.id);
+
+    // --- Redireccionar al Tema Del Link --- 
+    res.redirect(`/topics/${ link.topicId }`);
+}
 
