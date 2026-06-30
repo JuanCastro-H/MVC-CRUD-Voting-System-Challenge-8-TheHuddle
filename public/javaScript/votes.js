013977e73cafbff6978ff2e1c5f4be52eba11171
Juan Castro <juamcastroh@gmail.com>
2026-06-30 08:44:49 -0300

    feat: Implementar logica dinamica para los votos de un tema

---
+++ public/javaScript/votes.js
@@ -0,0 +1,25 @@
+// --- Obtener Botones Para Votar ---
+const topicButtons = document.querySelectorAll(".vote-topic");
+const linkButtons  = document.querySelectorAll(".vote-link");
+
+
+// --- Agregar Votacion A Cada Tema ---
+topicButtons.forEach(button => {
+
+    // --- Detectar Click Del Boton ---
+    button.addEventListener("click", async () => {
+
+        const id = button.dataset.id;  // Obtener ID del tema.
+
+        // --- Enviar Solicitud De Voto Al Servidor ---
+        const response = await fetch(`/topics/${id}/vote`, { method: "POST" });
+
+        // --- Obtener tema actualizado ---
+        const topic = await response.json(); 
+
+        // Actualizar Cantidad De Votos A la Vista ---
+        document.getElementById(`topic-votes-${id}`).textContent = topic.votes;
+    });
+});
+
+
