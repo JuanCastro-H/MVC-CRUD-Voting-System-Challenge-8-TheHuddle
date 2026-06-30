3d52a99faecd36af240aaf5fbac151f51ec7dd04
Juan Castro <juamcastroh@gmail.com>
2026-06-30 02:05:49 -0300

    feat: Importar modelos de Temas y Lins y crear logicaa para poder votar por un tema

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -1,4 +1,19 @@
-// --- Logica para ver los votos de los temas ---
+// --- Importar Modelo de Temas ---
+const Topic = require("../models/Topic");
+
+// --- Importar Modelo de Links --
+const Link = required("../models/Link");
+
+// --- Logica Para Votar Por un Tema ---
 exports.VoteTopic = (req, res) => {
-    console.log("Votos esta funcionando");
+
+    // --- Aumentar Voto del Tema ---
+    Topic.vote(req.params.id);
+
+    // --- Redireccionar a La Pagina de Temas ---
+    res.redirect("/topics");
+
 };
+
+
+
